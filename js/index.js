2fc6690702228d3af9e695e4d10bc3e1a0994edf
rockysasso <fernandezsassos@udesa.edu.ar>
2024-11-16 00:01:25 -0300

    Look Basico API

--- js/index.js
+++ js/index.js
@@ -1,2 +1,28 @@
+listaRecetas = document.querySelector(".indexRecepieContainer");
 
 fetch("https://dummyjson.com/recipes")
+.then(function(reponse) {
+    return reponse.json();
+})
+.then(function(data) {
+    let recepies = "";
+
+    for (let i = 0; i < 14; i++) {
+        const receta = data.recipes[i];
+        
+        recepies += `
+            <a href="./receta.html?id=${receta.id}">
+                <article>
+                    <img src="${receta.image}" alt="${receta.name}">
+                    <p>${receta.name}</p>
+                    <p>${receta.difficulty}</p>
+                </article>
+            </a>
+        `;
+    }
+
+    listaRecetas.innerHTML = recepies;
+})
+.catch(function(err) {
+    console.log("Error: " + err);
+});
