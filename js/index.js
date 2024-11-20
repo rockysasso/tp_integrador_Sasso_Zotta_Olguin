56cf0f23cc479b4fce24f02098e832a2f97875da
JZotta <jzotta@udesa.edu.ar>
2024-11-20 20:31:46 -0300

    Boton ver mas Funcional y terminado

--- js/index.js
+++ js/index.js
@@ -26,3 +26,39 @@ fetch("https://dummyjson.com/recipes")
 .catch(function(err) {
     console.log("Error: " + err);
 });
+
+let verMas = document.querySelector(".botonCargarmas");
+let i = 14; 
+
+verMas.addEventListener('click', function() {
+  let contador = i;
+
+  fetch("https://dummyjson.com/recipes")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data){
+      let recetasExtra = "";
+
+      for (let i = contador; i < contador + 10; i++) {
+        if (data.recipes[i]) {
+          const receta = data.recipes[i];
+          
+          recetasExtra += `
+            <a href="./receta.html?id=${receta.id}">
+                <article>
+                    <img src="${receta.image}" alt="${receta.name}">
+                    <p>${receta.name}</p>
+                    <p>${receta.difficulty}</p>
+                </article>
+            </a>
+          `;
+        }
+      }
+      listaRecetas.innerHTML += recetasExtra;
+      i += 10 
+      
+    })
+    .catch(function(err) {
+        console.log("Error: " + err);
+    })})
