1f01e6746d062c78df3ac96cf88d24123a17b153
ju1704 <jolguin@udesa.edu.ar>
2024-11-23 18:54:11 -0300

    commit formulario error

--- js/search-results.js
+++ js/search-results.js
@@ -1,18 +1,31 @@
-let searchForm = document.querySelector (".search-form");
-let searchBuscador = document.querySelector ("#bucador");
+let formulario = document.querySelector (".search-form");
+
+let buscador = document.querySelector ("#buscador");
 let errorMessage = document.querySelector ("#errorMessage");
 
-function muestraError(message) {
-    errorMessage.innerText = message;
-}
+let emptyErrorMessage = "El campo no puede estar vacio";
+let errorFormLength = "La contraseña debe tener al menos 3 caracteres";
 
-function corrigeError (){
-    errorMessage.innerText = "";
+let erroresBool = false;
 
-}
-
-searchBuscador.addEventListener ("searchBuscador", function(){
-    corrigeError();
-    if (searchBuscador.value.)
+formulario.addEventListener ("submit", function(event){
+    event.preventDefault();
+    if (buscador.value == ""){
+        errorMessage.style.display = "block";
+        errorMessage.innerText = emptyErrorMessage;
+        erroresBool = true
+    } else {
+    errorMessage.style.display = "none";
     }
-})
+    if (buscador.value.length < 3) {
+        errorMessage.style.display = "block";
+        errorMessage.innerText = errorFormLength;
+        erroresBool = true
+    } else{
+    errorMessage.style.display = "none";
+
+    } 
+     if (erroresBool == false){
+    this.submit();
+    }
+});
