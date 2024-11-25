86e5645b9529ea8359e5f16c5564620d1ef9152a
ju1704 <jolguin@udesa.edu.ar>
2024-11-25 17:19:33 -0300

    form buscador completo

--- js/search-results.js
+++ js/search-results.js
@@ -1,31 +1,36 @@
 let formulario = document.querySelector (".search-form");
 
 let buscador = document.querySelector ("#buscador");
-let errorMessage = document.querySelector ("#errorMessage");
+
+let errorMessageEmpty = document.querySelector (".errorMessageEmpty");
+let errorMessageLength = document.querySelector (".errorMessageLength");
 
 let emptyErrorMessage = "El campo no puede estar vacio";
 let errorFormLength = "La contraseña debe tener al menos 3 caracteres";
 
 let erroresBool = false;
 
+
+
 formulario.addEventListener ("submit", function(event){
     event.preventDefault();
-    if (buscador.value == ""){
-        errorMessage.style.display = "block";
-        errorMessage.innerText = emptyErrorMessage;
-        erroresBool = true
-    } else {
-    errorMessage.style.display = "none";
-    }
-    if (buscador.value.length < 3) {
-        errorMessage.style.display = "block";
-        errorMessage.innerText = errorFormLength;
-        erroresBool = true
-    } else{
-    errorMessage.style.display = "none";
 
-    } 
-     if (erroresBool == false){
+    if (buscador.value == ""){        
+        errorMessageEmpty.innerText = emptyErrorMessage;
+        errorMessageEmpty.style.display = "block";
+        erroresBool = true;
+    } else {
+    erroresBool = false;
+    };
+    if (buscador.value.length < 3) {        
+        errorMessageLength.innerText = errorFormLength;
+        errorMessageLength.style.display = "block";
+        erroresBool = true;
+    } else{
+    erroresBool = false;
+    };
+
+    if (erroresBool == false){
     this.submit();
     }
 });
